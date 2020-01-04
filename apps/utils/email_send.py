caa6b78d208e5402b948546520f8a293de9a3f78
ZREDU-007 <59474720+ZREDU-007@users.noreply.github.com>
2020-01-04 18:11:58 +0800

    Update email_send.py

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -33,21 +33,21 @@ def send_register_email(email, send_type="register"):
     email_body = ""
 
     if send_type == "register":
-        email_title = "汽车慕课学院注册激活链接"
+        email_title = "慕课学院注册激活链接"
         email_body = "请点击下面的链接激活你的账号: http://127.0.0.1:8000/active/{0}".format(code)
 
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
             pass
     elif send_type == "forget":
-        email_title = "汽车慕课学院注册密码重置链接"
+        email_title = "慕课学院注册密码重置链接"
         email_body = "请点击下面的链接重置密码: http://127.0.0.1:8000/reset/{0}".format(code)
 
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
         if send_status:
             pass
     elif send_type == "update_email":
-        email_title = "汽车慕课学院邮箱修改验证码"
+        email_title = "慕课学院邮箱修改验证码"
         email_body = "你的邮箱验证码为: {0}".format(code)
 
         send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
